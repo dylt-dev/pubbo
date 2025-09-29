fd1dc6707bf31b52732e44c5cc54daab44169584
Chris Lalos <chris.lalos@gmail.com>
2025-09-29 17:32:57 -0500

    tightened up error handling etc

--- cmd/rootCmd.go
+++ cmd/rootCmd.go
@@ -28,25 +28,35 @@ func runCmd (cmd *cobra.Command, args []string) error {
 	socketPath, _ := cmd.Flags().GetString("socket-path")
 	fmt.Printf("Pubbo'ing %s at %s ...\n", filePath, socketPath)
 
+	// Remove the socket. os.RemoveAll() returns nil if no socket
 	err := os.RemoveAll(socketPath)
 	if err != nil {
 		fmt.Println("error removing socket")
 		os.Exit(1)
 	}
+
+	// Listen for connections on socket
 	sock, err := net.Listen("unix", socketPath)
 	if err != nil {
 		fmt.Println("error opening socket")
 		os.Exit(1)
 	}
+
+	// Add a `defer` that closes the socket
+	// @note maybe this should remove the socket
 	defer func(sock net.Listener) {
 		fmt.Println("closing socket ...")
 		sock.Close()
 	}(sock)
+	
+	// chmod the rocket to read/write for owners, readonly for world
 	err = os.Chmod(socketPath, 0664)
 	if err != nil {
 		fmt.Println("error chmod'ing socket")
 		os.Exit(1)
 	}
+	
+	// Infinite loop: accept incoming connections + write the path's contents
 	for {
 		conn, err := sock.Accept()
 		if err != nil {
@@ -54,22 +64,51 @@ func runCmd (cmd *cobra.Command, args []string) error {
 			os.Exit(1)
 		}
 		fmt.Printf("Connection received. Sending content at %s ...\n", filePath)
-		pub(filePath, conn)
+		nWritten, err := pub(filePath, conn)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+		} else {
+			fmt.Printf("%d byte(s) written.\n", nWritten)
+		}
 	}
 }
 
 
-func pub (filePath string, conn net.Conn) {
+func pub (filePath string, conn net.Conn) (int64, error) {
+	var f *os.File
+	
+	// Add a `defer` to close the connection
+	defer func (conn net.Conn) {
+		var err error
+		if f != nil {
+			err = f.Close()
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "error closing file (%s)", filePath)
+			}
+		}
+
+		fmt.Println("Closing connection ...")
+		err = conn.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error closing connection")
+		}
+	}(conn)
+
+	// Try and open the file
+	// @note this aborts the file on temporary file unavailability
 	f, err := os.Open(filePath)
 	if err != nil {
-		fmt.Println("error opening file")
-		os.Exit(1)
+		fmt.Fprintf(os.Stderr, "error opening file (%s)\n", filePath)
+		return 0, err
 	}
-	defer f.Close()
-	defer func (conn net.Conn) {
-		fmt.Println("Closing connection ...")
-		conn.Close()
-	}(conn)
-	io.Copy(conn, f)
+
+	// @note this only writes once. if there's more to write, too bad.
+	var nWritten int64
+	nWritten, err = io.Copy(conn, f)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error copying data: %n byte(s) written\n", nWritten)		
+	}
+
+	return nWritten, nil
 }
 
